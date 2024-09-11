1a5f73f86da5e5a260674d211a914487153aac9a
Manuel Sanchez Suyon <msanchezs@valtx.pe>
2024-09-10 19:24:40 -0500

    improve style

--- random-topic-front/src/App.jsx
+++ random-topic-front/src/App.jsx
@@ -4,36 +4,42 @@ function App() {
   const URL_API = "http://localhost:8080";
 
   const [data, setData] = useState(null);
-  const [idUser, setIdUSer] = useState(6);
+  const [idUser, setIdUSer] = useState(1);
   const [isOpenCard, setIsOpenCard] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [typeCard, setTypeCard] = useState("");
 
   const getRandomCard = async (idUser) => {
-    let url = URL_API + "/random-card/" + idUser;
-    await fetch(url)
-      .then((response) => response.json())
-      .then((dataResponse) => setData(dataResponse))
-      .then((dataResponse) => {
-        if (data == null) return;
-        console.log(data);
-        setTitle(data["title"]);
-        setContent(data["content"]);
-        setTypeCard(data["type"]["name"]);
+    try {
+      const url = `${URL_API}/random-card/${idUser}`;
+      const response = await fetch(url);
+      if (!response.ok) throw new Error('Error fetching data');
+      
+      const dataResponse = await response.json();
+      console.log(dataResponse)
+      setData(dataResponse);
+
+      if (dataResponse) {
+        setTitle(dataResponse.title);
+        setContent(dataResponse.content);
+        setTypeCard(dataResponse.type.name);
         setIsOpenCard(true);
-      });
+      }
+    } catch (error) {
+      console.error("Error fetching random card:", error);
+    }
   };
 
   return (
-    <div className=" min-h-screen bg-slate-800">
-      <div className=" mx-auto my-auto max-w-xl p-3">
-        <h1 className="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          <span className=" text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+    <div className="min-h-screen bg-slate-800">
+      <div className="mx-auto my-auto max-w-xl p-3">
+        <h1 className="mb-4 pt-8 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl hover:scale-105 transition">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
             Interesting Conversations
           </span>
         </h1>
-        <p className="text-lg text-center font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        <p className="text-lg text-center font-normal text-gray-500 lg:text-xl dark:text-gray-400 py-2">
           Connect Deeper: Discover, Share, Engage with Cards!
         </p>
       </div>
@@ -41,7 +47,7 @@ function App() {
       <div>
         <button
           type="button"
-          className=" block mx-auto text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 hover:scale-110 transition"
+          className="block mx-auto text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 hover:scale-110 transition"
           onClick={() => getRandomCard(idUser)}
         >
           Random Topic
@@ -49,8 +55,8 @@ function App() {
       </div>
 
       {isOpenCard && (
-        <div className="max-w-sm mx-auto my-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          {typeCard == "question" && (
+        <div className="max-w-sm mx-auto my-8 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          {typeCard === "question" && (
             <svg
               className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3"
               aria-hidden="true"
@@ -65,7 +71,7 @@ function App() {
               />
             </svg>
           )}
-          {typeCard == "topic" && (
+          {typeCard === "topic" && (
             <svg
               className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3"
               aria-hidden="true"
@@ -80,7 +86,7 @@ function App() {
               />
             </svg>
           )}
-          {typeCard == "dynamic" && (
+          {typeCard === "dynamic" && (
             <svg
               className="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3"
               aria-hidden="true"
